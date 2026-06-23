df4caa012dad651e96fc2b96d3ef295d11ddc934
Ing. Eduardo Pérez Archundia <eduardo.perez.archundia@outlook.com>
2026-06-23 14:21:13 -0600

    J.A.R.V.I.S. | v0.3.0 ---- * Se agrega conexión inicial de paquetes del monorepo

---
+++ apps/sandbox-api/src/main.ts
@@ -0,0 +1,7 @@
+import { Jarvis } from '@jarvis/core';
+
+console.log('------------------------------');
+console.log(Jarvis.name);
+console.log(Jarvis.description);
+console.log('------------------------------');
+console.log('Version: ' + Jarvis.version);
---
+++ packages/core/src/index.ts
@@ -0,0 +1,5 @@
+export const Jarvis = {
+  name: 'J.A.R.V.I.S. | Pacakge - Core',
+  description: 'Package - Core for J.A.R.V.I.S.',
+  version: '0.0.0'
+} as const;
